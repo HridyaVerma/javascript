b523326482f18fa1139fd9a380c739453859927d
Hridya Verma <155651124+HridyaVerma@users.noreply.github.com>
2024-04-14 13:00:08 +0000

    comparison logic

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2>=1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null > 0)
+console.log(null == 0);
+console.log(null >= 0);  // null is converted into zero
+
+console.log(undefined > 0);
+console.log(undefined <=0);
+console.log(undefined < 0); 
+
+// we should avoid the above 6 comparison 
+
+
+// ===  strict check (not only check the value it even check the datatype)
+
+console.log("2" === 2)
