1abcf8c81c97bc578ac37e964d32ea11d48ff1ad
Hridya Verma <155651124+HridyaVerma@users.noreply.github.com>
2024-04-14 04:02:17 +0000

    basic dataypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treal all js code as newer version 
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3) 
+
+console.log("hridya")
+
+
+// for the doucmentation refer to mdn or tc39.es
+
+
+let name="hridya"  // string 
+let age=18  // number 
+let isloggedIn= false   //bool
+
+
+//number => 2^53
+//bigint =>
+// string => " "
+//boolean => true/false
+//null => stanalone value (representation of empty value)
+// null is a object
+// undefined => on value is assigned
+//symbol => unique
+
+//object
+
+console.log(typeof undefined)    //undefiend 
+console.log(typeof null) // object 
